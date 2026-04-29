f54c638112149c73b401aaa6885d2ecc46bd3ae5
karol <karol@KOMPUTEREK>
2026-04-29 21:32:08 +0200

    Add project files.

---
+++ EdytorIkon/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace IconEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.numericUpDownWidth = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownHeight = new System.Windows.Forms.NumericUpDown();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.listBoxIcons = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(117, 68);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(159, 22);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // numericUpDownWidth
+            // 
+            this.numericUpDownWidth.Location = new System.Drawing.Point(117, 123);
+            this.numericUpDownWidth.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownWidth.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownWidth.Name = "numericUpDownWidth";
+            this.numericUpDownWidth.Size = new System.Drawing.Size(160, 22);
+            this.numericUpDownWidth.TabIndex = 1;
+            this.numericUpDownWidth.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownHeight
+            // 
+            this.numericUpDownHeight.Location = new System.Drawing.Point(117, 176);
+            this.numericUpDownHeight.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownHeight.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownHeight.Name = "numericUpDownHeight";
+            this.numericUpDownHeight.Size = new System.Drawing.Size(160, 22);
+            this.numericUpDownHeight.TabIndex = 2;
+            this.numericUpDownHeight.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(117, 228);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(160, 28);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Add icon";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // listBoxIcons
+            // 
+            this.listBoxIcons.FormattingEnabled = true;
+            this.listBoxIcons.ItemHeight = 16;
+            this.listBoxIcons.Location = new System.Drawing.Point(68, 294);
+            this.listBoxIcons.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBoxIcons.Name = "listBoxIcons";
+            this.listBoxIcons.Size = new System.Drawing.Size(265, 212);
+            this.listBoxIcons.TabIndex = 4;
+            this.listBoxIcons.SelectedIndexChanged += new System.EventHandler(this.listBoxIcons_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(419, 68);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(431, 357);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBoxIcons);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.numericUpDownHeight);
+            this.Controls.Add(this.numericUpDownWidth);
+            this.Controls.Add(this.textBoxName);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.NumericUpDown numericUpDownWidth;
+        private System.Windows.Forms.NumericUpDown numericUpDownHeight;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ListBox listBoxIcons;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ EdytorIkon/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IconEditor
+{
+    public partial class Form1 : Form
+    {
+        DBDataContext db = new DBDataContext();
+        private const int pixelSize = 20;
+        private Graphics graph;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if(textBoxName.Text.Length > 3 && numericUpDownWidth.Value > 10 && numericUpDownHeight.Value > 10)
+            {
+                Icon i = new Icon();
+                i.Name = textBoxName.Text;
+                i.Height = (int)numericUpDownHeight.Value;
+                i.Width = (int)numericUpDownWidth.Value;
+
+                db.Icons.InsertOnSubmit(i);
+                db.SubmitChanges();
+                LoadIconsFromDatabase();
+            }
+            else
+            {
+                MessageBox.Show("Please provide valid data! (Name > 3 chars, Dimensions > 10)");
+            }
+        }
+        private void LoadIconsFromDatabase()
+        {
+            listBoxIcons.Items.Clear();
+            listBoxIcons.Items.AddRange(db.Icons.ToArray());
+        }
+        private void listBoxIcons_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBoxIcons.SelectedItem is Icon)
+            {
+                Icon i = listBoxIcons.SelectedItem as Icon;
+                pictureBox1.Image = new Bitmap(i.Width * pixelSize+1, i.Height * pixelSize+1);
+                graph = Graphics.FromImage(pictureBox1.Image);
+                graph.Clear(Color.LightYellow);
+                RenderIconCanvas();
+            }
+        }
+        private void RenderIconCanvas()
+        {
+            if (listBoxIcons.SelectedItem is Icon)
+            {
+                Icon i = listBoxIcons.SelectedItem as Icon;
+                graph.Clear(Color.White);
+                foreach(IconPoint ip in i.IconPoints)
+                {
+                    graph.FillRectangle(new SolidBrush(Color.FromArgb(ip.Color)), ip.X*pixelSize, ip.Y*pixelSize, pixelSize, pixelSize);
+                }
+                for(int x=0; x <= i.Height; x++)
+                {
+                    graph.DrawLine(new Pen(Color.Black), 0, x*pixelSize, i.Width*pixelSize, x*pixelSize);
+                }
+                for (int y = 0; y <= i.Width; y++)
+                {
+                    graph.DrawLine(new Pen(Color.Black), y * pixelSize, 0, y* pixelSize, i.Height * pixelSize);
+                }
+                pictureBox1.Refresh();
+            }
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (listBoxIcons.SelectedItem is Icon)
+            {
+                Icon i = listBoxIcons.SelectedItem as Icon;
+                if (e.Button == MouseButtons.Left)
+                {
+                    IconPoint ic;
+                    ic = i.IconPoints.Where(x => x.X == e.X / pixelSize && x.Y == e.Y / pixelSize).FirstOrDefault();
+   
+                    if(ic==null)
+                    {
+                    ic = new IconPoint();
+                    ic.X = e.X / pixelSize;
+                    ic.Y = e.Y / pixelSize;
+                        ic.Icon = i;
+                    }
+
+                    ic.Color = Color.Purple.ToArgb();
+                    db.SubmitChanges();
+                    RenderIconCanvas();
+                }
+            }
+        }
+    }
+    partial class Icon
+    {
+        public override string ToString()
+        {
+            return $"{Name} ({Width} x {Height})";
+        }
+    }
+}
